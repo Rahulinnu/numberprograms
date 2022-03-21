856a9bbffd0a1d25b96f46a6c7c8f357dffcbf67
rahul unni <rahul@rahulsmac.local>
2022-03-21 19:12:23 +0530

    Num program added

---
+++ numpro/Fibonacci.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numpro
+{
+    public class Fibonacci
+    {
+        public static void Series()
+        {
+            int first=0, second=1, res = 0;
+            int length = 0;
+            Console.WriteLine("Please write n value for fibonacci series ");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.Write(first + "" + second);
+
+            Console.Write(" " + res);
+            for(int i =1; i<=10; i++)
+            {
+                first = second;
+                second = res;
+                res = first + second;
+                Console.Write(" " + res);
+
+            }
+        }
+    }
+}
---
+++ numpro/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numpro
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please choose number to execute");
+            Console.WriteLine("1:Fibonacci \n 2:Exit");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    Fibonacci.Series();
+                    break;
+
+                case 2:
+                    return;
+
+                default:
+                    Console.WriteLine("Invalid");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
